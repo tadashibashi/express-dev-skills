d4386a0055b6ea03c59cacc8cc733ada3a2aad00
Aaron Ishibashi <a.ishibashi.music@gmail.com>
2023-06-29 20:59:10 -0700

    add update and delete functions to Skill model

--- models/Skill.js
+++ models/Skill.js
@@ -1,19 +1,25 @@
 
 // mock database
 const skills = [
-    {_id: "fed24812feabc35124", name: "HTML5", learned: true, icon: "fa-brands fa-html5"},
-    {_id: "fed24812feabc35125", name: "CSS3", learned: true, icon: "fa-brands fa-css3-alt"},
-    {_id: "fed24812feabc35126", name: "JavaScript", learned: false, icon: "fa-brands fa-js"},
-    {_id: "fed24812feabc35127", name: "Node.js", learned: false, icon: "fa-brands fa-node-js"},
-    {_id: "fed24812feabc35128", name: "Express.js", learned: false, icon: "fa-solid fa-truck-fast"},
+    {_id: 12345, name: "HTML5", learned: true, icon: "fa-brands fa-html5"},
+    {_id: 12346, name: "CSS3", learned: true, icon: "fa-brands fa-css3-alt"},
+    {_id: 12347, name: "JavaScript", learned: false, icon: "fa-brands fa-js"},
+    {_id: 12348, name: "Node.js", learned: false, icon: "fa-brands fa-node-js"},
+    {_id: 12349, name: "Express.js", learned: false, icon: "fa-solid fa-truck-fast"},
 ];
+
+// Ticket number for new ids
+let currentId = 12350;
+
 
 /**
  * Returns the skill or null if it doesn't exist
+ * @param id {number} - id of the skill
  */ 
 function getOne(id) {
     return skills.find(skill => skill._id === id) || null;
 }
+
 
 /**
  * Get skills array
@@ -23,7 +29,63 @@ function getAll() {
 }
 
 
+/**
+ * Add a skill to the db
+ * @param skillName {string}
+ */
+function addOne(skillName) {
+    skills.push({
+        _id: currentId++,
+        icon: "",
+        name: skillName,
+        learned: false,
+    });
+}
+
+
+/**
+ * Delete a skill with a certain id from the db
+ * @param id {number}
+ * @returns {boolean} true if skill with id was deleted, or false if not.
+ */
+function deleteOne(id) {
+    for (let i = 0; i < skills.length; ++i) {
+        if (skills[i]._id === id) {
+            skills.splice(i, 1);
+            return true;
+        }
+    }
+
+    return false;
+}
+
+/**
+ * Update a pre-existing skill
+ * @param id {number}        - id of the skill to update
+ * @param skillName {string} - new name of the skill
+ * @param learned {boolean}  - whether skill learned
+ * @param icon {string}      - font awesome icon class
+ * @returns {boolean} whether skill was updated
+ */
+function updateOne(id, skillName, learned, icon) {
+    const skill = getOne(id);
+
+    if (skill) {
+        skill.name = skillName;
+        skill.learned = learned;
+        skill.icon = icon;
+        return true;
+    } else {
+        console.error(`[Skill updateOne] Attempted to update skill with ${id}, but skill with id was not found`);
+        return false;
+    }
+}
+
+
 module.exports = {
     getOne,
     getAll,
+    addOne,
+    deleteOne,
+    updateOne,
 };
